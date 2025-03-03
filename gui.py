d1838810b106a7ba543d731b0bc95575feaef0a6
vbcl <vbcl.mail.com>
2025-03-03 11:10:17 +0300

    add key names to buttons

--- gui.py
+++ gui.py
@@ -10,13 +10,13 @@ sg.theme('Black')
 current_time = sg.Text('', key='current_time')
 label = sg.Text("Type in a to-do:")
 input_box = sg.InputText(tooltip='Enter a to-do', key='todo')
-add_button = sg.Button("Add", size=7)
+add_button = sg.Button("Add", size=7, key='add')
 
 list_box = sg.Listbox(values=functions.get_todos(), size=(57, 10), key='todos', enable_events=True)
-edit_button = sg.Button('Edit', size=7)
+edit_button = sg.Button('Edit', size=7, key='edit')
 
-complete_button = sg.Button('Complete', size=9)
-exit_button = sg.Button('Exit', size=7)
+complete_button = sg.Button('Complete', size=9, key='complete')
+exit_button = sg.Button('Exit', size=7, key='exit')
 
 window = sg.Window('TODO LIST',
                    layout=[[current_time],
@@ -29,14 +29,14 @@ while True:
     event, values = window.read(timeout=500)
     window['current_time'].update(value=time.strftime("%d.%m.%Y %H:%M"))
     match event:
-        case 'Add':
+        case 'add':
             todos = functions.get_todos()
             todos.append(values['todo'].capitalize() + '\n')
             functions.update_todo_list(todos)
 
             window['todos'].update(values=todos)  # update listbox
 
-        case 'Edit':
+        case 'edit':
             try:
                 todo_to_edit = values['todos'][0]
                 new_todo = values['todo']
@@ -54,7 +54,7 @@ while True:
         case 'todos':  # update input window whenever user click in list box
             window['todo'].update(value=values['todos'][0])
 
-        case 'Complete':
+        case 'complete':
             try:
                 todos = functions.get_todos()
                 todo_to_complete = values['todos'][0]
@@ -67,7 +67,7 @@ while True:
                 sg.popup("Please, select an item first.", title='Warning!', font=('Helvetica', 12),
                          button_justification='right')
 
-        case 'Exit':
+        case 'exit':
             break
 
         case sg.WINDOW_CLOSED:
