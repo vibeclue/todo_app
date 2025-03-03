178e37009a4d8523ab1f6deaca9d871d1eb49d46
vbcl <vbcl.mail.com>
2025-03-03 09:48:56 +0300

    add current time to app window, fix bug out of range list todo

--- gui.py
+++ gui.py
@@ -1,7 +1,11 @@
+from ast import Index
+
 import functions
 import FreeSimpleGUI as sg
+import time
 
 
+current_time = sg.Text('', key='current_time')
 label = sg.Text("Type in a to-do:")
 input_box = sg.InputText(tooltip='Enter a to-do', key='todo')
 add_button = sg.Button("Add", size=7)
@@ -13,14 +17,15 @@ complete_button = sg.Button('Complete', size=7)
 exit_button = sg.Button('Exit', size=7)
 
 window = sg.Window('TODO LIST',
-                   layout=[[label, input_box, add_button],
+                   layout=[[current_time],
+                           [label, input_box, add_button],
                            [list_box, edit_button],
                            [complete_button, exit_button]],
                    font=('Helvetica', 14))
 
 while True:
-    event, values = window.read()
-    print(event, values, sep='\n')
+    event, values = window.read(timeout=500)
+    window['current_time'].update(value=time.strftime("%d.%m.%Y %H:%M"))
     match event:
         case 'Add':
             todos = functions.get_todos()
@@ -30,27 +35,35 @@ while True:
             window['todos'].update(values=todos)  # update listbox
 
         case 'Edit':
-            todo_to_edit = values['todos'][0]
-            new_todo = values['todo']
+            try:
+                todo_to_edit = values['todos'][0]
+                new_todo = values['todo']
 
-            todos = functions.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo + '\n'
-            functions.update_todo_list(todos)
+                todos = functions.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                functions.update_todo_list(todos)
 
-            window['todos'].update(values=todos)  # update listbox
+                window['todos'].update(values=todos)  # update listbox
+            except IndexError:
+                sg.popup("Please, select an item first.", title='Warning!', font=('Helvetica', 12),
+                         button_justification='right')
 
         case 'todos':  # update input window whenever user click in list box
             window['todo'].update(value=values['todos'][0])
 
         case 'Complete':
-            todos = functions.get_todos()
-            todo_to_complete = values['todos'][0]
-            todos.remove(todo_to_complete)
-            functions.update_todo_list(todos)
+            try:
+                todos = functions.get_todos()
+                todo_to_complete = values['todos'][0]
+                todos.remove(todo_to_complete)
+                functions.update_todo_list(todos)
 
-            window['todo'].update(value='')       # clear input field
-            window['todos'].update(values=todos)  # update listbox
+                window['todo'].update(value='')       # clear input field
+                window['todos'].update(values=todos)  # update listbox
+            except IndexError:
+                sg.popup("Please, select an item first.", title='Warning!', font=('Helvetica', 12),
+                         button_justification='right')
 
         case 'Exit':
             break
